0dc976234456b7e198828c495e84d0c999a21a5c
ZeeKotomori <zikriaf23@gmail.com>
2023-11-08 23:12:46 +0700

    middleware

---
+++ src/index.js
@@ -0,0 +1,34 @@
+const express = require('express');
+
+const app = express();
+const PORT = 3001;
+
+app.use((req, res, next) => { //next adalah fungsi yang harus dipanggil untuk melanjutkan eksekusi ke middleware berikutnya atau ke penanganan rute jika diperlukan.
+    console.log(`${req.method}:${req.url}`); //${req.method} mengambil metode HTTP dari objek permintaan, seperti "GET," "POST," atau "PUT.", ${req.url} mengambil URL permintaan.
+    next(); // fungsi next(), yang mengindikasikan bahwa middleware ini telah selesai menjalankan tugasnya dan harus melanjutkan eksekusi ke middleware berikutnya
+});
+
+app.listen(PORT, () => console.log(`server run on port ${PORT}`)); //port
+const drinks = [ //obejct data
+    {
+    item: 'milk',
+    quantity: 2
+    },
+    {
+    item: 'coffee',
+    quantity: 2
+    },
+    {
+    item: 'tea',
+    quantity: 2
+    }
+]
+app.get('/drinks', (req, res) => { //get a object data
+    res.send(drinks);
+});
+
+app.post('/drinks', (req , res) => { //created a object data
+    console.log(req.body);
+    drinks.push(req.body); // dataType = JSON
+    res.send(201);
+});
