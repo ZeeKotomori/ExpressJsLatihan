69edc82c392b1328f874b48d32d3eb05fe06e919
ZeeKotomori <zikriaf23@gmail.com>
2023-11-09 22:08:44 +0700

    add Route Parameters

--- src/index.js
+++ src/index.js
@@ -30,6 +30,13 @@ app.get('/drinks', (req, res) => { //get a object data
     res.send(drinks);
 });
 
+app.get('/drinks/:item', (req, res) => { //item = set parameter, :item adalah parameter dinamis yang dapat diambil dari URL. Saat sebuah permintaan HTTP diterima, Express.js dapat menangkap nilai yang diberikan pada bagian URL yang sesuai dengan parameter tersebut.
+    // console.log(req.params.item); //item = get parameter
+    const { item } = req.params; //Dalam kode const { item } = req.params;, kita menggunakan destructuring assignment untuk mengekstrak nilai dari properti item dalam objek req.params. `{ item }` = const item = req.params.item;
+    const drinksItem = drinks.find((g) => g.item === item); //const drinksItem = drinks.find((g) => g.item === item);, kita menggunakan metode find() untuk mencari objek dalam array drinks yang memiliki properti item yang sama dengan nilai dari parameter dinamis yang diambil dari URL. (g) => g.item === item adalah fungsi untuk memeriksa apakah properti item dari object dalam array drinks nilainya sama dengan :item, bila ada akan memunculkan data, bila tidak akan memunculkan undifined.
+    res.send(drinksItem);
+});
+
 app.post('/drinks', (req , res) => { //created a object data
     console.log(req.body);
     drinks.push(req.body); // dataType = JSON
